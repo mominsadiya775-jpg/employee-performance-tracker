5346b701f15ddc809c5813106f78b63d5fd22d71
Sadiya Momin <mominsadiya775@gmail.com>
2026-05-23 03:14:07 -0700

    Fixed mysql port

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
 app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
 app.config['MYSQL_DB'] = os.environ.get('MYSQLDATABASE')
-app.config['MYSQL_PORT'] = int(os.environ.get('MYSQLPORT'))
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQLPORT', 44015))
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 mysql = MySQL(app)
