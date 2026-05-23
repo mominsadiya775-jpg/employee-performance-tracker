edd9dbeeec1fa1c57e60301d5d22d406f90cf9c5
Sadiya Momin <mominsadiya775@gmail.com>
2026-05-23 02:39:29 -0700

    Fixed MySQL env variables

--- backend/app.py
+++ backend/app.py
@@ -8,11 +8,11 @@ import io
 app = Flask(__name__)
 CORS(app)
 
-app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST', 'kodama.proxy.rlwy.net')
-app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER', 'root')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD', 'OoIfUYWpxFTIZfUzVuudYrvBmocsUhmd')
-app.config['MYSQL_DB'] = os.environ.get('MYSQLDATABASE', 'railway')
-app.config['MYSQL_PORT'] = int(os.environ.get('MYSQLPORT', 44015))
+app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQLDATABASE')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQLPORT'))
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 mysql = MySQL(app)
